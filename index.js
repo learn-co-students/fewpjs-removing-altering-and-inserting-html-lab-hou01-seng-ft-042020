92dbdc2408c7595be46d613de3a6a555c6d71a4f
lilirdz <lilimrod@gmail.com>
2020-05-31 19:44:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
-document.querySelector("main#main").remove();
+document.getElementById("main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Lili is the champion";
