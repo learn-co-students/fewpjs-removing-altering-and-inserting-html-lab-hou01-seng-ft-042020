db19dfc0dc79aa02339cae6eda7fa9e9835cf1c1
devchea <rchea817@gmail.com>
2020-05-31 23:06:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+const name = "Richard"
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = `${name} is the champion`;
