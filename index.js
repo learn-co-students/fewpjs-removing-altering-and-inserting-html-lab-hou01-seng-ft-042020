6e7dab292d0a742a7d899bfe57a97440bd79b565
eiphyllis <phyllisgetcity@gmail.com>
2020-06-06 14:02:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'PHYLLIS is the champion';
